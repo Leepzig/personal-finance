40f4b9fbbf4f06863917559de380cf692afbddbb
andrewsimmons <neon.duncan@gmail.com>
2021-10-18 18:03:50 -0500

    added requestingReducer

--- src/reducers/requestingReducer.js
+++ src/reducers/requestingReducer.js
@@ -1,1 +1,12 @@
+const initialState = true
 
+export const requestingReducer = (state=initialState, action) => {
+    switch(action.type) {
+        case "REQUESTING":
+            return true
+        case "FINISHED_REQUESTING":
+            return false
+        default:
+            return state
+    }
+}
