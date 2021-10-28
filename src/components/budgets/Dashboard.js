cba81cbccd0114859e9ad31a9be5299980a97524
andrewsimmons <neon.duncan@gmail.com>
2021-10-28 16:26:54 -0500

    added a drop down for selecting budgets

--- src/components/budgets/Dashboard.js
+++ src/components/budgets/Dashboard.js
@@ -1,4 +1,4 @@
-import { Button, Typography } from '@mui/material'
+import { Button, MenuItem, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import Budget from './Budget'
@@ -7,9 +7,13 @@ import { createBudget, loadAllBudgets, setBudget } from '../../actions/budgetAct
 import { useForm } from '../../hooks/useForm'
 import { Input } from '@mui/material'
 import { Box } from '@mui/system'
-import { useHistory } from 'react-router-dom'
-import BasicTabs from './BasicTabs'
 import Errors from '../static/Errors'
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { InputLabel } from '@mui/material'
+import { clearErrors } from '../../actions/errorAction'
+
 
 const Dashboard = () => {
     const currentUser = useSelector(state => state.sessions.currentUser)
@@ -18,17 +22,12 @@ const Dashboard = () => {
     const dispatch = useDispatch()
     const [form, handleForm, resetForm] = useForm({header:""})
     const [hiddenState, setHiddenState ] = useState(true)
-    const history = useHistory()
-    //Should I keep track of this in state?
-    // let loadedBudget = false
-    const handleDisplayBudgetClick = budget => {
-        dispatch(setBudget(budget))
-        dispatch(loadAllBudgets())
-    }
+    const [currentBudget, setCurrentBudget] = useState("")
 
     const handleNewBudgetClick = () => {
         setHiddenState(!hiddenState)
     }
+
     const handleNewBudgetSubmit = e => {
         e.preventDefault()
         dispatch(createBudget(form, currentUser.id))
@@ -36,13 +35,19 @@ const Dashboard = () => {
     }
     
     useEffect(()=> {
-            dispatch(loadAllBudgets())
-            return () => {
-                dispatch({type:"CLEAR_ERRORS"})
-            }
+        dispatch(loadAllBudgets())
+        return () => {
+            dispatch({type:"CLEAR_ERRORS"})
+        }
     }, [dispatch])
 
-
+    const handleDisplayBudgetClick = e => {
+        dispatch(setBudget(e.target.value))
+        dispatch(loadAllBudgets())
+        debugger
+        setCurrentBudget(e.target.value.header)
+        dispatch(clearErrors())
+    }
     
     if (!loggedIn) return (
         <div>
@@ -59,13 +64,23 @@ const Dashboard = () => {
             { hiddenState ? "" : <Button variante="text" size="small" type="submit" >Submit New Budget</Button>}
             
             </Box>
-            <ul>
-                {budgets.map( budget => <li key={budget.id} onClick={() => handleDisplayBudgetClick(budget)}>{budget.header}</li>)}
-            </ul>
-            {/* <BasicTabs /> */}
+            <FormControl sx={{ m: 1, minWidth: 200 }}>
+                <InputLabel id="budget-selector">Budget Label</InputLabel>
+                <Select
+                labelId="budget-selector"
+                id="budget-selector"
+                value={currentBudget}
+                label="Age"
+                onChange={handleDisplayBudgetClick}
+                >
+                    {budgets.map(budget=> {
+                        return <MenuItem value={budget}>{budget.header}</MenuItem>}
+                    )}
+                </Select>
+                <FormHelperText>Choose a Budget or create a new one.</FormHelperText>
+            </FormControl>
             <hr/>
             <Budget />
-            {/* { loadedBudget ? < Budget /> : null } */}
         </div>
     )
 }
